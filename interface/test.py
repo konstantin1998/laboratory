f52b753c225d06acb425c61368af6f6326428a28
konstantin1998 <galperin.15@yandex.ru>
2020-07-27 11:40:49 +0300

    add image cropping

--- interface/test.py
+++ interface/test.py
@@ -1,41 +1,24 @@
 from PIL import Image
 import os
 
-
-def scale_image(input_image_path,
-                output_image_path,
-                width=None,
-                height=None
-                ):
-    original_image = Image.open(input_image_path)
-    w, h = original_image.size
-    print('The original image size is {wide} wide x {height} '
-          'high'.format(wide=w, height=h))
-
-    if width and height:
-        max_size = (width, height)
-    elif width:
-        max_size = (width, h)
-    elif height:
-        max_size = (w, height)
-    else:
-        # No width or height specified
-        raise RuntimeError('Width or height required!')
-
-    original_image.thumbnail(max_size, Image.ANTIALIAS)
-    original_image.save(output_image_path)
-
-    scaled_image = Image.open(output_image_path)
-    width, height = scaled_image.size
-    print('The scaled image size is {wide} wide x {height} '
-          'high'.format(wide=width, height=height))
-
-
-inputdir = "D:/Word/big_img/DS20180906120034_Full46786_2Conv_V0_60_R0_200_T0.5e6_Tc1-20200715T191756Z-001/DS20180906120034_Full46786_2Conv_V0_60_R0_200_T0.5e6_Tc1"
-outputdir = "D:/Word/big_img/DS20180906120034_Full46786_2Conv_V0_60_R0_200_T0.5e6_Tc1-20200715T191756Z-001/scaled"
+inputdir = "D:/Word/DataSets/DS_1205/Migr/DS20180906120502_Full46786_2Conv_Migr_V0_60_R0_200_T0.5e6_Tc1"
+outputdir = "D:/Word/DataSets/DS_1205/Migr/cropped"
 img_names = os.listdir(inputdir)
-w, h = 3449, 7866
-for name in img_names:
-    input_path = os.path.join(inputdir, name)
-    output_path = os.path.join(outputdir, name)
-    scale_image(input_path, output_path, w//2, h//2)
+for img_name in img_names:
+    img_path = os.path.join(inputdir, img_name)
+    img = Image.open(img_path)
+    w, h = img.size
+    left_top_box = (0, 0, w // 2, h // 2)
+    right_top_box = (w // 2 + 1, 0, w, h // 2)
+    left_bottom_box = (0, h // 2, w // 2, h)
+    right_bottom_box = (w // 2 + 1, h // 2, w, h)
+
+    name, ext = os.path.splitext(img_name)
+    left_top_img = img.crop(left_top_box)
+    left_top_img.save(os.path.join(outputdir, name + '_lt' + ext))
+    right_top_img = img.crop(right_top_box)
+    right_top_img.save(os.path.join(outputdir, name + '_rt' + ext))
+    left_bottom_img = img.crop(left_bottom_box)
+    left_bottom_img.save(os.path.join(outputdir, name + '_lb' + ext))
+    right_bottom_img = img.crop(right_bottom_box)
+    right_bottom_img.save(os.path.join(outputdir, name + '_rb' + ext))
